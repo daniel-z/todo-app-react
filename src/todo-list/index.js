036aa8532b7364a73e945d896e6fcf089614255e
Daniel Zamorano <dzamorano@tacitknowledge.com>
2018-05-01 00:50:18 -0500

    Using pure functions for list component.

--- src/todo-list/index.js
+++ src/todo-list/index.js
@@ -3,30 +3,23 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
-class TodoList extends Component {
+const getTaskItem = (task) => (
+    <li key={task.id}>
+        {task.complete === true ? 'complete' : 'incomplete' } {" "} / {task.description} ({task.id})
+    </li>
+);
 
-  getTaskItem(task) {
-    return (
-        <li key={task.id}>
-            {task.complete === true ? 'complete' : 'incomplete' } - {task.description} ({task.id})
-        </li>
-    );
-  }
+const renderTasks = (tasks) => (
+    tasks.map((task) => getTaskItem(task))
+);
 
-  renderTasks(tasks) {
-      return tasks.map((task) => this.getTaskItem(task));
-  }
-
-  render() {
-    return (
-      <div className="todo-app__list">
-        <ul>
-          { this.renderTasks(this.props.tasks) }
-        </ul>
-      </div>
-    );
-  }
-}
+const TodoList = (props) => (
+  <div className="todo-app__list">
+    <ul>
+      { renderTasks(props.tasks) }
+    </ul>
+  </div>
+);
 
 TodoList.propTypes = {
     tasks: PropTypes.array
